50d442d62a8c4ee657a6ff9a9f948fa088660399
huangjunlin <9579711+huangjunlin511024@user.noreply.gitee.com>
2023-09-19 21:35:29 +0800

    event.js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+// print process.argv
+// process.argv.forEach((val, index) => {
+//   console.log(`${index}: ${val}`);
+// });
+// console.log(process.env)
+// process.stdin.setEncoding('utf8');
 
+// process.stdin.on('readable', () => {
+//   const chunk = process.stdin.read();
+//   if (chunk !== null) {
+//     process.stdout.write(`data: ${chunk}`);
+//   }
+// });
 
+// process.stdin.on('end', () => {
+//   process.stdout.write('end');
+// });
 const EventEmitter = require('events');
-
-
-
-const myEmitter = new EventEmitter();
-myEmitter.on('event', () => {
-  console.log('触发了一个事件！');
+/*1:声明变量*/
+var num1, num2;
+/*2：向屏幕输出，提示信息，要求输入num1*/
+process.stdout.write('请输入num1的值：');
+/*3：监听用户的输入*/
+process.stdin.on('data', function (chunk) {
+    if (!num1) {
+        num1 = Number(chunk);
+        /*4：向屏幕输出，提示信息，要求输入num2*/
+        process.stdout.write('请输入num2的值:');
+    } else {
+        num2 = Number(chunk);
+        process.stdout.write('结果是：' + (num1 + num2));
+    }
 });
-myEmitter.emit('event');
